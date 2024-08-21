38f4e013ee9273f34fbf734f2e237fa47701d07b
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2024-08-21 08:26:15 +0530

    feat: implemented router provider in app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
+import { RouterProvider } from "react-router-dom";
+import router from "./routes/router";
+
 function App() {
   return (
-    <>
-      <h1>Cuboid Map</h1>
-    </>
+    <RouterProvider router={router} fallbackElement={<p>Initial Load...</p>} />
   );
 }
 
