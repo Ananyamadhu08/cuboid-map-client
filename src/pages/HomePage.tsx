ae6c6d33a95b81ffae0f4379e829ab5148bfedcc
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2024-08-23 02:13:17 +0530

    feat: implemented basic layout

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,49 @@
-const HomePage = () => {
+import React from "react";
+import Card from "../components/Card";
+import Tabs from "../components/Tabs";
+import Layout from "../layout/Layout";
+
+const Home: React.FC = () => {
+  const tabData = [
+    {
+      label: "Map Vew",
+      content: (
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      ),
+    },
+    {
+      label: "3D Cuboid View",
+      content: (
+        <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
+          <Card />
+          <Card />
+          <Card />
+          <Card />
+        </div>
+      ),
+    },
+  ];
+
   return (
-    <>
-      <h1>Home Page</h1>
-    </>
+    <Layout>
+      <div className="mb-4">
+        <Tabs tabs={tabData} />
+      </div>
+    </Layout>
   );
 };
 
-export default HomePage;
+export default Home;
