c1ed316936935b455b42d0ab21f91fdd36ca4755
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2024-08-23 02:06:37 +0530

    feat: created card component

---
+++ src/components/Card.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Card: React.FC = () => {
+  return (
+    <div className="h-32 rounded-lg border-2 border-dashed border-gray-300 md:h-64 dark:border-gray-600"></div>
+  );
+};
+
+export default Card;
