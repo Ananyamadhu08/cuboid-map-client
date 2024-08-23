56460f73012dba7565f7ec630dbc3e7413637830
Ananyamadhu08 <78725970+Ananyamadhu08@users.noreply.github.com>
2024-08-23 11:38:44 +0530

    feat: integrated map slice in store

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authSlice from "../slices/authSlice";
+import mapSlice from "../slices/mapSlice";
 
 export const store = configureStore({
   reducer: {
     auth: authSlice,
+    map: mapSlice,
   },
 });
 
